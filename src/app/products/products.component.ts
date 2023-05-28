7efb8b7c50544739391c16453b228bca951db6c9
MatiurRasel <matiurrasel1002@gmail.com>
2023-05-28 17:38:21 +0600

    updated

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  view: 'grid' | 'list' =  'list';
+  sortBy: 'default' | 'htl' | 'lth' = 'default';
+
+  constructor() {
+
+  }
+  
+  ngOnInit(): void {
+    
+  }
 
 }
